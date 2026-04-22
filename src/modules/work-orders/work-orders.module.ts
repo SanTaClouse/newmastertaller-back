5027fb31fe858947dd0c82280a7df8aa3ba124a5
santiagoriv06 <santiagoriv06@gmail.com>
2026-04-22 00:08:25 -0300

    fix: register VehicleMileageLog and Client in WorkOrdersModule

--- src/modules/work-orders/work-orders.module.ts
+++ src/modules/work-orders/work-orders.module.ts
@@ -5,11 +5,13 @@ import { WorkOrdersService } from './work-orders.service';
 import { WorkOrder } from './work-order.entity';
 import { WorkOrderPhaseLog } from './work-order-phase-log.entity';
 import { Vehicle } from '../vehicles/vehicle.entity';
+import { VehicleMileageLog } from '../vehicles/vehicle-mileage-log.entity';
 import { Expense } from '../expenses/expense.entity';
 import { RepairPhase } from '../repair-phases/repair-phase.entity';
+import { Client } from '../clients/client.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WorkOrder, WorkOrderPhaseLog, Vehicle, Expense, RepairPhase])],
+  imports: [TypeOrmModule.forFeature([WorkOrder, WorkOrderPhaseLog, Vehicle, VehicleMileageLog, Expense, RepairPhase, Client])],
   controllers: [WorkOrdersController],
   providers: [WorkOrdersService],
   exports: [WorkOrdersService],
